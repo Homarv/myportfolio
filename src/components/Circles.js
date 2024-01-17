9c37842e309a9f5aaf4e4ac20bb130f965997af2
Homarv <62882518+Homarv@users.noreply.github.com>
2024-01-17 12:09:08 +0100

    add css to mobile

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Circles = () => {
   return (
     <div className='container'>
-    <div className="box">
       <div className="circle"></div>
-    </div>
     </div>
   );
 };

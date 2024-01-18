c61cf0b986c501ff0daf243147b6a73e9186ff60
Homarv <62882518+Homarv@users.noreply.github.com>
2024-01-18 15:00:18 +0100

    modified active to bnt

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Btn = ({ text, customClass }) => {
+  const [isBtnTouched, setIsBtnTouched] = useState(false);
+
+  const handleTouchStart = () => {
+    setIsBtnTouched(true);
+  };
+
+  const handleTouchEnd = () => {
+    setIsBtnTouched(false);
+  };
+
   return (
     <div className='btn-container'>
-      <div className={`btn ${customClass}`}>
+      <div
+        className={`btn ${customClass} ${isBtnTouched ? 'touched' : ''}`}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
         {text}
       </div>
     </div>
